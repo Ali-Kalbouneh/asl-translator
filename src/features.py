f8b602d7438cc4fab03d2cefc654cd8893db70bb
Ali-Kalbouneh <alikalbouneh1@gmail.com>
2025-11-27 22:05:46 -0500

    Landmark Normalization

--- src/features.py
+++ src/features.py
@@ -5,14 +5,18 @@ def normalize_landmarks(landmarks):
     
     points = np.array(landmarks, dtype=np.float32)
 
+    # Use wrist as base
     wrist = points[0]
     points -= wrist
 
+    # Use distance between writs to upper hand as hand size
     scale = np.linalg.norm(points[9])
-
+ 
+    # If scale is too small, default to 1
     if scale < 1e-6:
         scale = 1.0
     
+    # Normalize
     points = points / scale
 
     return points.flatten()

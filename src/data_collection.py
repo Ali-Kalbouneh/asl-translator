144bf00d700e63596d9d01ad8bef6c4f0e77bbf8
Ali-Kalbouneh <alikalbouneh1@gmail.com>
2025-11-28 02:50:16 -0500

    Added compatibily to type in text as label, instead of being limited to only letters.

--- src/data_collection.py
+++ src/data_collection.py
@@ -23,6 +23,8 @@ if not os.path.exists(csv_path):
         writer.writerow(["label"] + [f"f{i}" for i in range(63)])
 
 current_label = None
+typing_label = False
+typed_text = ""
 
 save_message = ""
 save_message_counter = 0
@@ -57,14 +59,34 @@ while True:
         cv2.putText(frame, save_message, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
         save_message_counter -= 1
 
+    if typing_label:
+        cv2.putText(frame, f"Label: {typed_text}_", (20, 430), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
     cv2.imshow("ASL Data Collection", frame)
 
     key = cv2.waitKey(1) & 0xFF
 
-    # Select which letter data is about to be collected on
-    if ord("a") <= key <= ord("z"):
+    # Select which letter data is about to be collected ona
+    if not typing_label and ord("a") <= key <= ord("z"):
         current_label = chr(key).upper()
         print(f"Current label set to: {current_label}")
+
+    elif key == ord("0") and not typing_label:
+        typing_label = True
+        typed_text = ""
+        print("Enter your own label, then press Enter")
+
+    elif typing_label:
+        if key == 13: #Enter
+            current_label = typed_text.strip()
+            typing_label = False
+            print(f"Custom label set to {current_label}")
+
+        elif key in (8, 127): # Backspace
+            typed_text = typed_text[:-1]
+
+        elif 32 <= key <= 126:
+            typed_text += chr(key)
 
     # save data to csv
     elif key == ord("3"):
